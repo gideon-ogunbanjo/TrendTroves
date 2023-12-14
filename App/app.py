b5d71430f992d17f9b05309dcd532dff6ec962da
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-12-14 10:04:52 +0100

    initialized model deployment with streamlit

---
+++ App/app.py
@@ -0,0 +1,12 @@
+# Libraries
+import streamlit as st
+import pickle as pkl
+import PIL
+import joblib as jbl
+import json
+
+st.set_page_config(
+    layout = 'centered',
+    initial_sidebar_state = 'collapsed',
+    page_title = 'TrendTroves'
+)
