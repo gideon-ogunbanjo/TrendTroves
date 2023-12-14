35b9301211119921d6e22e55b17795b6670d9ac9
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-12-14 10:19:50 +0100

    made minor changes

--- App/app.py
+++ App/app.py
@@ -19,34 +19,48 @@ with open(model_file, 'rb') as file:
 
 # Loading column information
 columns_file = 'App/columns.json'
-with open(columns_file, 'r') as file:
-    columns = json.load(file)['data_columns']
-    
-# Prediction Function
+try:
+    with open(columns_file, 'r') as file:
+        columns = json.load(file)['data_columns']
+except (json.JSONDecodeError, KeyError) as e:
+    st.error(f"Error loading column information from 'columns.json': {str(e)}")
+    columns = None
+
 def predict_price(location, sqft, bath, bedrooms):
-    loc_index = np.where(columns == location)[0][0]
+    if columns is None or not columns:
+        st.error("Error loading column information. Please check 'columns.json'.")
+        return None
+
+    loc_index = np.where(np.array(columns) == location)[0]
+    
+    if not loc_index:
+        st.error("Invalid location selected. Please choose a valid location.")
+        return None
+
+    loc_index = loc_index[0]
 
     input_data = np.zeros(len(columns))
     input_data[0] = sqft
     input_data[1] = bath
     input_data[2] = bedrooms
-    if loc_index >= 0:
-        input_data[loc_index] = 1
+    input_data[loc_index] = 1
 
     return model.predict([input_data])[0]
 
 # Streamlit app
 def main():
-    st.title('TrendTroves - House Price Prediction Model')
+    st.title('TrendTroves - India House Price Prediction Model')
 
-    location = st.selectbox('Select Location', columns[3:])
-    sqft = st.slider('Total Square Feet Area', 500, 5000, 1500)
-    bath = st.slider('Number of Bathrooms', 1, 5, 2)
-    bedrooms = st.slider('Number of Bedrooms', 1, 5, 2)
+    if columns is not None:
+        location = st.selectbox('Select Location', columns[3:])
+        sqft = st.slider('Total Square Feet Area', 500, 5000, 1500)
+        bath = st.slider('Number of Bathrooms', 1, 5, 2)
+        bedrooms = st.slider('Number of Bedrooms', 1, 5, 2)
 
-    if st.button('Predict Price'):
-        result = predict_price(location, sqft, bath, bedrooms)
-        st.success(f'Predicted Price: ₹{result:.2f} Lakhs')
+        if st.button('Predict Price'):
+            result = predict_price(location, sqft, bath, bedrooms)
+            if result is not None:
+                st.success(f'Predicted Price: ₹{result:.2f} Lakhs')
 
 if __name__ == '__main__':
     main()
